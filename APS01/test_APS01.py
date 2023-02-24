82f5c568db93119e420442db605e5cddca6c11b6
Enricco Gemha <riccogemha@gmail.com>
2023-02-24 18:34:25 -0300

    autograder finalizado

--- APS01/test_APS01.py
+++ APS01/test_APS01.py
@@ -2,6 +2,8 @@ import cv2
 import numpy as np
 from ex1 import equaliza
 from ex2 import realca_caixa_vermelha
+from ex3 import recorta_leopardo
+from ex4 import antartida, canada
 
 # courtesy of: https://pyimagesearch.com/2014/09/15/python-compare-two-images/
 def mse(imageA, imageB):
@@ -33,8 +35,45 @@ def test_ex1_compare_img():
 def test_ex2():
     img_given = cv2.imread("APS01/img/cena_canto_sala.jpg")
     img_submitted = realca_caixa_vermelha(img_given)
-    img_submitted = img_submitted[:,:,::-1]
+    try:
+        img_submitted = img_submitted[:,:,::-1]
+    except:
+        img_submitted = img_submitted[:,::-1]
     img_expected = cv2.imread("APS01/expected_img/ex2.jpg")
     err = mse(img_submitted, img_expected)
     print(err)
     assert err < 1
+
+def test_ex3():
+    img_given = cv2.imread("APS01/img/ex3_recorte_leopardo.png")
+    img_submitted = recorta_leopardo(img_given)
+    try:
+        img_submitted = img_submitted[:,:,::-1]
+    except:
+        img_submitted = img_submitted[:,::-1]
+    img_expected = cv2.imread("APS01/expected_img/ex3_case1.png")
+    err = mse(img_submitted, img_expected)
+    if err >= 1:
+        img_expected = cv2.imread("APS01/expected_img/ex3_case2.png")
+        err = mse(img_submitted, img_expected)
+    print(err)
+    assert err < 1
+
+def test_ex4():
+    img_given = cv2.imread("APS01/img/ant_canada_250_160.png")
+    img_submitted1 = antartida(img_given)
+    img_submitted2 = canada(img_given)
+    try:
+        img_submitted1 = img_submitted1[:,:,::-1]
+        img_submitted2 = img_submitted2[:,:,::-1]
+    except:
+        img_submitted1 = img_submitted1[:,::-1]
+        img_submitted2 = img_submitted2[:,::-1]
+    img_expected1 = cv2.imread("APS01/expected_img/ex4_ant.png")
+    img_expected2 = cv2.imread("APS01/expected_img/ex4_can.png")
+    err1 = mse(img_submitted1, img_expected1)
+    err2 = mse(img_submitted2, img_expected2)
+    print(err1)
+    print(err2)
+    assert err1 < 1
+    assert err2 < 1
